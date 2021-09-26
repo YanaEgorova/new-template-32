f94ea519cecc2712657727edd76778ff0fec868a
Yana <yanaegorova69.3@gmail.com>
2021-09-26 21:37:55 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,162 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Thermometer',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Fever Indicator',
+                text: 'Fever alarm when the peak temperature has registered higher than 37.5°C. With memory function, auto display the last measured value after power on, help you easily track the fevers progress.',
+            },
+            {
+                title: 'High Sensitive Thermometer',
+                text: 'The LCD digital thermometer is designed delicately ,is high sensitive and accuracy to measure , measurement range is 32℃-42.9℃（89.6 ℉-109.2 ℉）, temperature error (high precision ) ≤ ± 0.1℃/32.1℉ (Between 36.0℃/96.8℉-39.0℃/102.2℉).',
+            },
+            {
+                title: 'Three Measurements Modes',
+                text: 'The thermometer takes the temperature in different ways: ORAL, RECTAL and UNDER THE ARM. Its soft silicone tip design make it possible to use the thermometer in any situation for adults/kids.',
+            },
+            {
+                title: 'Waterproof Tip',
+                text: 'The waterproof tip enables you to clean the thermometer regularly in an easy way. Disinfecting the tip before and after each use is recommended for hygiene.',
+            },
+            {
+                title: 'Safe & Professional Service',
+                text: ' It’s safer than mercury glass thermometers measured in the mouth or rectal.Your baby also can use it properly. It\'s definitely a home care product you need.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Bed Sheet Set',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: '100% Cotton',
+            },
+            {
+                text: 'Sheet set includes (1) 102x90 inch flat sheet, (1) 80x60 inch fitted sheet and (2) 20x30 inch standard pillowcases.',
+            },
+            {
+                text: 'Lightweight all-season sheets are exceptionally soft, breathable, and comfortable; woven with a pre-dyed yarn for a combed cotton heathered look.',
+            },
+            {
+                text: 'Jersey knit stretch provides a perfectly snug fit on your mattress; fitted sheet pockets fit mattresses up to 14-inches deep.',
+            },
+            {
+                text: 'Machine washable for easy care.',
+            },
+            {
+                text: 'Easy care; machine wash and tumble dry.',
+            },
+        ],
+        images: ['./img/img2-1.jpg', './img/img2-2.jpg', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Head Shaver',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                title: '5 Dual Track Heads',
+                text: 'Get a fast, close shave with 66% more cutting heads. The dual track heads can achieve a smooth, shaven head in under two minutes.',
+            },
+            {
+                title: 'Maximum Skin Contact',
+                text: 'The shaver head is designed for multidirectional Pivot, flex and contour, delivering a close, smooth shave.',
+            },
+            {
+                title: 'Hair Capture Chamber',
+                text: 'Cut hair collects in the chamber underneath the shaver heads for easy disposal and cleanup.',
+            },
+            {
+                title: '100% Waterproof',
+                text: 'So you can comfortably shave in the shower, and with or without gel or cream.',
+            },
+            {
+                title: 'Lithium Power',
+                text: 'Up to 50 minutes of cordless runtime with the lithium rechargeable battery.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Muffler',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Durable fully welded 16 gauge aluminized steel.',
+            },
+            {
+                text: 'Excellent Street/Strip and Off Road application.',
+            },
+            {
+                text: 'No internal packing to blowout.',
+            },
+            {
+                text: 'Race proven Delta Flow technology.',
+            },
+            {
+                text: 'Easy Installation.',
+            },
+        ],
+        images: ['./img/img4-1.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Wireless Headphone',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Bluetooth 5. 0 technology for effortlessly reliable connectivity including AAC and AptX Low Latency codec support.',
+            },
+            {
+                text: 'Active noise cancellation for uninterrupted listening pleasure.',
+            },
+            {
+                text: '30-hour battery life with USB-C fast charging.',
+            },
+            {
+                text: 'Sennheiser App support providing an equalizer, Podcast Mode and Firmware updates.',
+            },
+            {
+                text: 'Included components: HD 450BT wireless headphones, audio cable USB-C charging cable, carry case, quick guide & safety guide.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Massage Table',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Beech Hardwood Frame / Multiple Frame Support Cables / Threaded Support Bar.',
+            },
+            {
+                text: '2” High Density Foam Deck / Water & Oil Resistant PU Leather Upholstery.',
+            },
+            {
+                text: 'Tabletop Dimensions: 73” x 28” / Height (Adjustable): 25.5” x 33.5” / Folded/Closed Table: 37” x 28” x 7”.',
+            },
+            {
+                text: 'Table Weight: 30 lbs / 400 lbs Working Weight Capacity.',
+            },
+            {
+                text: 'INCLUDED ACCESSORIES: Face Cradle (SC-514), Face Cushion (SC-524), Sheet Set (SC-508), Threaded Support Bar, 2-Year Limited Warranty.',
+            },
+            {
+                text: 'IMPORTANT: Please refer to all current photos and descriptions for product accuracy. Any photos/details in the Customer Reviews section may be dated/inaccurate due to modifications over time.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png', './img/img6-5.png', './img/img6-6.png', './img/img6-7.png', './img/img6-8.png'],
+    },
+]
